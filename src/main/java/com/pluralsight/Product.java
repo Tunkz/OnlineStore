aac27a312afb86f6e0c696de0b2de858efd9278b
Tunkz <Bubatunkz00@gmail.com>
2023-10-20 14:05:57 -0400

    Finished setting up constructor,getter&setter

---
+++ src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+public class Product {
+    private String name;
+    private String id;
+    private double price;
+    private String department;
+
+    public Product(String name, String id, double price, String department) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", price=" + price +
+                ", department='" + department + '\'' +
+                '}';
+    }
+/*--getter/setter--*/
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+}
